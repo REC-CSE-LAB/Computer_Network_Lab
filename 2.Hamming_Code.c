d9b54d4bda5093995f8a28b03a92afd9db3b7fce
iamwatchdogs <shamith301102@gmail.com>
2022-09-03 18:55:47 +0530

    Modified 2.Hamming_Code.c : Fixed a Bug

--- 2.Hamming_Code.c
+++ 2.Hamming_Code.c
@@ -35,13 +35,20 @@ int main()
     trailer[2] = output[3] ^ output[4] ^ output[5] ^ output[6];
     trailer[3] = output[7] ^ output[8] ^ output[9] ^ output[10];
 
-    index = trailer[0] + trailer[1] * 2 + trailer[2] * 4 + trailer[3] * 16;
+    index = trailer[0] + trailer[1] * 2 + trailer[2] * 4 + trailer[3] * 8;
 
-    printf("There is an error in %d bit position in the data sequence.", index);
+    if (index - 1 > 0 && index - 1 < 11)
+    {
+        printf("There is an error in %d bit position in the data sequence.", index);
 
-    output[index - 1] ^= output[index - 1];
+        output[index - 1] ^= output[index - 1];
 
-    printf("\nAfter correction of the error, the data sequence is : ");
+        printf("\nAfter correction of the error, ");
+    }
+    else
+        printf("There is no error in the data sequence, ");
+
+    printf("The data sequence is : ");
 
     for (int i = 0; i < 11; i++)
         printf("%d ", output[i]);
@@ -51,8 +58,18 @@ int main()
 
 // Input/Output:-
 // ------------
-//     Enter the 7-bit message: 1011001     /* Input */
-//     The 11-bit data sequence at the sender: 1 0 1 0 0 1 1 1 0 0 1
-//     Enter the 11-bit data sequence at receiver: 10100101001      /* Input */
-//     There is an error in 7 bit position in the data sequence.
-//     After correction of the error, the data sequence is : 1 0 1 0 0 1 0 1 0 0 1
+
+//     case 1:
+//     -------
+//          Enter the 7-bit message: 1011001
+//          The 11-bit data sequence at the sender: 1 0 1 0 0 1 1 1 0 0 1
+//          Enter the 11-bit data sequence at receiver: 10100101001
+//          There is an error in 7 bit position in the data sequence.
+//          After correction of the error, The data sequence is : 1 0 1 0 0 1 0 1 0 0 1
+
+//      case 2:
+//      -------
+//          Enter the 7-bit message: 1011001
+//          The 11-bit data sequence at the sender: 1 0 1 0 0 1 1 1 0 0 1
+//          Enter the 11-bit data sequence at receiver: 10100111001
+//          There is no error in the data sequence, The data sequence is : 1 0 1 0 0 1 1 1 0 0 1
